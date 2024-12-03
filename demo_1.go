cee61216f4cf6c618579e50ee9017ea2219be6c6
nishaodong <416363428@qq.com>
2024-12-03 20:52:42 +0800

    测试记录提交

---
+++ demo_1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name string = "Tom"
+	fmt.Println(name)
+
+	const age = 30
+	fmt.Println(age)
+
+	const name_1, name_2 string = "Tom", "Jay"
+	fmt.Println(name_1, name_2)
+
+	const name_3, age_1 = "Tom", 30
+	fmt.Println(name_3, age_1)
+
+	fmt.Print("输出到控制台不换行")
+	fmt.Println("---")
+	fmt.Println("输出到控制台并换行")
+	fmt.Printf("name=%s,age=%d\n", "Tom", 30)
+	fmt.Printf("name=%s,age=%d,height=%v\n", "Tom", 30, fmt.Sprintf("%.2f", 180.567))
+}
---
+++ eth/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
+
+	address := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+
+	fmt.Println(address.Hex())        // 0x71C7656EC7ab88b098defB751B7401B5f6d8976F
+	fmt.Println(address.Hash().Hex()) // 0x00000000000000000000000071c7656ec7ab88b098defb751b7401b5f6d8976f
+	fmt.Println(address.Bytes())      // [113 199 101 110 199 171 136 176 152 222 251 117 27 116 1 181 246 216 151 111]
+}
