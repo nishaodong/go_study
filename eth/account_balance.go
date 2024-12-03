d5a65a9f1d03226b7b6cfa58b90427c9b9774455
nishaodong <416363428@qq.com>
2024-12-03 20:57:56 +0800

    测试记录提交

---
+++ eth/account_balance.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0x6782E7e24814eebd1e9147babfA7125a219C1EF6")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+
+	blockNumber := big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
+}
